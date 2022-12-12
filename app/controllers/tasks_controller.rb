fe9ac66d8618da5eaaaaa9190b5678e82b23c16d
yoichi.mihara <pandanokokoro41@gmail.com>
2022-12-12 04:41:35 +0000

    mod tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,25 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show]
+  
   def index
-    @tasks = Task.all
+    if logged_in?
+      puts current_user[:id]
+      #@tasks = Task.find_by(user_id: 4)
+      @tasks = Task.all
+      @tasks = current_user.tasks
+      # @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
+    end
   end
   
   def show
-    @task = Task.find(params[:id])
+    @user = current_user[:id]
+    @exe_user = Task.find(params[:id]).user_id
+
+    if @user == @exe_user
+      @task = Task.find(params[:id])
+    else
+      redirect_to root_url
+    end
   end
   
   def new
@@ -13,7 +28,7 @@ class TasksController < ApplicationController
   
   def create
     # puts params
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = "Taskが正常に登録されました"
@@ -51,6 +66,12 @@ class TasksController < ApplicationController
   
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def ensure_correct_user
+    @task = Task.find(params[:id])
+    unless @task.user_id == current_user[:id]
+    end
   end
 end
 
